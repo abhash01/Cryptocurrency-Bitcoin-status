b740b98b0a2423eeb408379acddfed3e77d62a4c
abhash01 <abhash01@gmail.com>
2020-12-23 01:35:18 +0530

    bugs

--- src/Coin.js
+++ src/Coin.js
@@ -23,9 +23,9 @@ const Coin = ({
           <p className='coin-volume'>${volume.toLocaleString()}</p>
 
           {priceChange < 0 ? (
-            <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+            <p className='coin-percent red'>{priceChange}%</p>
           ) : (
-            <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
+            <p className='coin-percent green'>{priceChange}%</p>
           )}
 
           <p className='coin-marketcap'>
